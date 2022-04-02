501f2e359214ec0801fad835b6fd2e7d0825e23b
Administrator <pmosoft@gmail.com>
2022-04-02 19:10:20 +0900

    챠트 기능 추가 plot_predict_scenario_store_family(self, scenario_id, store_nbr, family2) plot_predict_scenario_store(self, scenario_id, store_nbr) plot_predict_store_family(self, store_nbr, family2) plot_train_store(self, store_nbr) plot_train_store_family(self, store_nbr, family2)

--- scenario_predict_visualization.py
+++ scenario_predict_visualization.py
@@ -1,49 +1,76 @@
 import util.file_util as file_util
 import util.plot_util as plot_util
-
+from feature import load_data
 
 class Scenario_predict_visualization:
     def __init__(self):
         print('>>> __init__')
         self.predict_df = file_util.read_jsons_to_pandas('d:/lge/pycharm-projects/kaggle_store_sales/output/')
+        self.trans = load_data.train_master2()
 
-    def plot_store_family(self, scenario_id, store_nbr, family2):
+    def plot_predict_scenario_store_family(self, scenario_id, store_nbr, family2):
         df = self.predict_df
         df2 = df[(df['scenario_id'] == scenario_id) & (df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
         title = scenario_id + "(" + str(store_nbr) + ")" + "(" + str(family2) + ")" + "(" + str(df2['score'].values[0]) + ")"
         test_x = list(range(len(df2['test_y'].values[0])))
         test_y = df2['test_y'].values[0]
         predict_y = df2['predict_y'].values[0]
-        plot_util.pyplot_01(title, 'date', 'sales', test_x, test_y, predict_y)
+        plot_util.pyplot_predict_scenario_store_family(title, 'date', 'sales', test_x, test_y, predict_y)
 
-    def plot_store_family_train(self, scenario_id, store_nbr, family2):
-        df = self.predict_df
-        df2 = df[(df['scenario_id'] == scenario_id) & (df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
-        title = scenario_id + "(" + str(store_nbr) + ")" + "(" + str(family2) + ")" + "(" + str(df2['score'].values[0]) + ")"
-        test_x = list(range(len(df2['test_y'].values[0])))
-        test_y = df2['test_y'].values[0]
-        predict_y = df2['predict_y'].values[0]
-        plot_util.pyplot_01(title, 'date', 'sales', test_x, test_y, predict_y)
-
-    def plot_store(self, scenario_id, store_nbr):
+    def plot_predict_scenario_store(self, scenario_id, store_nbr):
         df = self.predict_df
         df2 = df[(df['scenario_id'] == scenario_id) & (df['store_nbr'] == store_nbr)]
-        plot_util.pyplot_02(df2)
+        plot_util.pyplot_predict_scenario_store(df2)
+
+    def plot_predict_store_family(self, store_nbr, family2):
+        df = self.predict_df
+        df2 = df[(df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
+        plot_util.pyplot_predict_store_family(df2)
+
+    def plot_train_store(self, store_nbr):
+        df = self.trans
+        df2 = df[(df['store_nbr'] == store_nbr)]
+        plot_util.pyplot_train_store(df2)
+
+    def plot_train_store_family(self, store_nbr, family2):
+        df = self.trans
+        df2 = df[(df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
+        title = "(" + str(store_nbr) + ")" + "(" + str(family2) + ")"
+        #plot_util.plotly_df(df2, title, 'date', 'sales')
+        plot_util.pyplot_train_store_family(df2, title, 'date', 'sales')
+
 
 # %%
 ###########################################################
 # execute
 ###########################################################
-# visual = Scenario_predict_visualization()
+#visual = Scenario_predict_visualization()
 
 # %%
-# visual.plot_store_family('x001d001y001m004', 1, 7)
-# visual.plot_store('x001d001y001m003', 2)
+#visual.plot_train_store(1)
+#visual.plot_train_store_family(1, 9)
+#visual.plot_predict_scenario_store_family('x001d001y001m004', 1, 7)
+#visual.plot_predict_scenario_store('x001d001y001m003', 2)
+#visual.plot_predict_store_family(1,4)
 # %%
 ###########################################################
 # test
 ###########################################################
 # %%
+#
+# df = visual.trans
+# store_nbr = 1; family2 = 4
+# df2 = df[(df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
+# title = "(" + str(store_nbr) + ")" + "(" + str(family2) + ")"
+# train_x = df2['date8']
+# train_y = df2['sales']
+# # predict_y = df2['predict_y'].values[0]
+# plot_util.pyplot_03(title, 'date', 'sales', train_x, train_y)
+#
+
+# %%
+#aa = df[(df['store_nbr'] == 1)]
+#df.columns
 # df = file_util.read_jsons_to_pandas(path)
 # df2 = df[(df['scenario_id'] == 'x001d001y001m001') & (df['store_nbr'] == 1) & (df['family2'] == 10)]
 # print(df2['score'].head(1))
--- util/plot_util.py
+++ util/plot_util.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import plotly.express as px
+# import file_util as file_util
 
-def pyplot_01(t1,xlabel,ylabel,x1,y1,y2):
+def pyplot_predict_scenario_store_family(t1,xlabel,ylabel,x1,y1,y2):
     plt.rcParams['font.family'] = 'Malgun Gothic'
     plt.figure(figsize=(10,5))
     plt.title(t1, fontsize=14)
@@ -15,7 +17,7 @@ def pyplot_01(t1,xlabel,ylabel,x1,y1,y2):
     plt.plot(x1, y2, color='r', alpha=0.6, marker='o', markersize=5, linestyle='--')
     plt.show()
 
-def pyplot_02(df):
+def pyplot_predict_scenario_store(df):
     plt.rcParams['font.family'] = 'Malgun Gothic'
     fig, axes = plt.subplots(7, 5, figsize=(20, 20))
 
@@ -33,7 +35,115 @@ def pyplot_02(df):
     fig.tight_layout()
     plt.show()
 
+def pyplot_predict_store_family(df):
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+    fig, axes = plt.subplots(3, 5, figsize=(20, 20))
+
+    inx = 0
+    for index, df2 in df.iterrows():
+        t1 = "(" + str(df2['scenario_id']) + ")" + "(" + str(df2['store_nbr']) + ")" + "(" + str(df2['family2']) + ")" + "(" + str(df2['score']) + ")"
+        x1 = df2['date']
+        y1 = df2['test_y']
+        y2 = df2['predict_y']
+        axes[inx // 5, inx % 5].grid()
+        axes[inx // 5, inx % 5].set(title=t1, xlabel='date', ylabel='sales')
+        axes[inx // 5, inx % 5].plot(x1, y1, color='b', alpha=0.6, marker='o', markersize=5, linestyle='-')
+        axes[inx // 5, inx % 5].plot(x1, y2, color='r', alpha=0.6, marker='o', markersize=5, linestyle='--')
+        inx = inx + 1
+
+    fig.tight_layout()
+    plt.show()
+
+def pyplot_train_store(df):
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+    fig, axes = plt.subplots(7, 5, figsize=(20, 20))
+
+    for family2 in range(33) :
+        df2 = df[(df['family2'] == family2+1)]
+        t1 = "(" + str(df2['store_nbr'].values[0]) + ")" + "(" + str(family2 + 1) + ")"
+        #x1 = list(range(len(df2['date'])))
+        x1 = df2['date']
+        y1 = df2['sales']
+        axes[family2 // 5, family2 % 5].grid()
+        axes[family2 // 5, family2 % 5].set(title=t1, xlabel='date', ylabel='sales')
+        axes[family2 // 5, family2 % 5].plot(x1, y1, color='b', alpha=0.6, marker='o', markersize=5, linestyle='-')
+
+    fig.tight_layout()
+    plt.show()
+
+def pyplot_train_store_family(df,title,x,y):
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+    plt.title(title)
+    plt.plot(df[x], df[y], color='b', alpha=0.6, marker='o', markersize=5, linestyle='-')
+    plt.show()
+
+def plotly_df(df,title,x,y):
+    fig = px.line(df, x=x, y=y, title=title)
+    fig.show()
+
+
+#%%
+###########################################################
+# test
+###########################################################
+
+# #%%
+# from feature import load_data
+# df = load_data.train_master2()
+#
+# #%%
+# df2 = df[(df['store_nbr'] == 1)]
+#
+# #%%
+#
 # path = "d:/lge/pycharm-projects/kaggle_store_sales/output/"
 # df = file_util.read_jsons_to_pandas(path)
-# df2 = df[(df['scenario_id'] == 'x001d001y001m001') & (df['store_nbr'] == 1)]
-# pyplot_02(df2)
+# #%%
+# df2 = df[(df['store_nbr'] == 1) & (df['family2'] == 2)]
+# pyplot_predict_store_family(df2)
+#  #%%
+#
+# #%%
+# loop1 = list(range(len(df2['scenario_id'])))
+# loop2 = list(range(len(df2)))
+#
+# # pyplot_store_train(df2)
+# #%%
+#
+# plt.rcParams['font.family'] = 'Malgun Gothic'
+# fig, axes = plt.subplots(3, 5, figsize=(20, 20))
+#
+# inx = 0
+# for index, df3 in df2.iterrows():
+#     t1 = "(" + str(df2['scenario_id'].values[0]) + ")" + "(" + str(df2['store_nbr'].values[0]) + ")" + "(" + str(df2['family2'].values[0]) + ")" + "(" + str(df2['score'].values[0]) + ")"
+#     x1 = list(range(len(df3['test_y'])))
+#     y1 = df3['test_y']
+#     y2 = df3['predict_y']
+#     axes[inx // 5, inx % 5].grid()
+#     axes[inx // 5, inx % 5].set(title=t1, xlabel='date', ylabel='sales')
+#     axes[inx // 5, inx % 5].plot(x1, y1, color='b', alpha=0.6, marker='o', markersize=5, linestyle='-')
+#     axes[inx // 5, inx % 5].plot(x1, y2, color='r', alpha=0.6, marker='o', markersize=5, linestyle='--')
+#     inx = inx + 1
+#
+# fig.tight_layout()
+# plt.show()
+#
+# #%%
+# # store_nbr = 1; family2 = 4
+# # df2 = df[(df['store_nbr'] == store_nbr) & (df['family2'] == family2)]
+# # title = "(" + str(store_nbr) + ")" + "(" + str(family2) + ")"
+# # train_x = df2['date8']
+# # train_y = df2['sales']
+# # # predict_y = df2['predict_y'].values[0]
+# # pyplot_03(title, 'date', 'sales', train_x, train_y)
+# #
+# # #%%
+# # fig = px.line(df2, x='date', y='sales', title='Daily total sales of the stores')
+# # fig.show()
+# # #%%
+# #
+# # plotly_01(df2,'date','sales','Daily total sales of the stores')
+# # path = "d:/lge/pycharm-projects/kaggle_store_sales/output/"
+# # df = file_util.read_jsons_to_pandas(path)
+# # df2 = df[(df['scenario_id'] == 'x001d001y001m001') & (df['store_nbr'] == 1)]
+# # pyplot_02(df2)
